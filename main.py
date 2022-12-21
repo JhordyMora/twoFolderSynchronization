8910b81c8c8758b2e20ae2ac0746c21061d3aa1b
JhordyMora <jhordy.mora@web.de>
2022-12-21 22:17:26 +0100

    fixing new logic

--- main.py
+++ main.py
@@ -6,53 +6,74 @@ import time
 
 def main():
     # input necesary for the program
-    PATH_SOURCE = input("Provide the real path and name of your source file: ")
-    PATH_REPLICA = input("Provide the real path where do you want to save replica file: ")
+    PATH_SOURCE = input("Provide the complete path of your source file: ")
+    PATH_REPLICA = input("Provide the complete path where do you want to save replica file: ")
+    
     try:
         SYNCHRO_TIME = int(input("Provide how regularly do you want to synchronize your replica file in minutes: "))
     except:
         print("Pleased, check if your type an integer/ number")
         print("Type all the information again")
         main()
-    
-    LOG_PATH = PATH_REPLICA + "log.txt"
-    try:
-        logCreation(LOG_PATH)
-    except:
-        informationForLog("Log file was already created. You tried to create a copy")
-        print("Log file was already created")
         
-    #Checking if the files exist or not
-    if os.path.isfile(os.path.realpath(PATH_SOURCE)):
+    PARENT_DIR = "/".join(PATH_REPLICA.split("/")[0:-1])
+    
+    LOG_PATH = PARENT_DIR + "/log.txt"
+    #Checking if the source folder exist
+    if os.path.isdir(os.path.realpath(PATH_SOURCE)):
         # Checking if the target folder exists
         if os.path.isdir(os.path.realpath(PATH_REPLICA)):
-            # Checking if a back up file exists
-            NAME_FILE = PATH_SOURCE.split("/")
-            PATH_BACK_UP_FILE = PATH_REPLICA +"backUp_"+ NAME_FILE[-1]
-            if os.path.isfile(os.path.realpath(PATH_BACK_UP_FILE)):
-                equal_data = compare2file(PATH_SOURCE, PATH_BACK_UP_FILE)
-                print(equal_data)
-            else:
-                creationBackUpFile(PATH_BACK_UP_FILE, LOG_PATH)
+            print("todo")
+            # # Checking if a back up file exists
+            # NAME_FILE = PATH_SOURCE.split("/")
+            # PATH_BACK_UP_FILE = PATH_REPLICA +"backUp_"+ NAME_FILE[-1]
+            # if os.path.isfile(os.path.realpath(PATH_BACK_UP_FILE)):
+            #     equal_data = compare2file(PATH_SOURCE, PATH_BACK_UP_FILE)
+            #     print(equal_data)
+            # else:
+            #     creationBackUpFile(PATH_BACK_UP_FILE, LOG_PATH)
         else:
-            creationReplicaFolder(PATH_REPLICA)
+            creationReplicaFolder(PATH_REPLICA, LOG_PATH)
+            # Creation of log file
+            LOG_PATH = PATH_REPLICA + "/log.txt"
+            try:
+                logCreation(LOG_PATH)
+            except:
+                informationForLog("Log file was already created. You tried to create a copy",LOG_PATH)
+                print("Log file was already created")
     else:
-        creationSourceFile(PATH_SOURCE)
+        creationSourceFolder(PATH_SOURCE, LOG_PATH)
+        creationReplicaFolder(PATH_REPLICA, LOG_PATH)
+        # Creation of log file
+        try:
+            logCreation(LOG_PATH)
+        except:
+            informationForLog("Log file was created",LOG_PATH)
+            print("Log file was created")
     
-def creationSourceFile(PATH_SOURCE):
-        answer = input("The File does not exist. Do you want to create it? [y] for Yes, or [n] for No ")
+def creationSourceFolder(PATH_SOURCE, LOG_PATH):
+    try:
+        answer = input("The Source Folder does not exist. Do you want to create it? [y] for Yes, or [n] for No ")
         if answer == "y":
             NAME_FILE = PATH_SOURCE.split("/")
-            print(f"the name of your file will be {NAME_FILE[-1]}")
-            source_file = open(PATH_SOURCE, "x")
-            source_file.close()
-            print("The file was created")
+            directory = NAME_FILE[-1]
+            PARENT_DIR = NAME_FILE[0:-1]
+            parent_dir = "/".join(PARENT_DIR)
+            os.chdir(parent_dir)
+            os.mkdir(directory)
+            informationForLog(f"The folder {directory} was created", LOG_PATH)
+            print(f"The folder {directory} was created")
         else:
-            print("Sorry we can not continue with the synchronization")
+            print("Sorry we can not continue with the synchronization. Please try again")
+            main()
+    except:
+        print("Are you sure did you write a file path? Please try again")
+        main()
             
-def creationReplicaFolder(PATH_REPLICA):
+def creationReplicaFolder(PATH_REPLICA, LOG_PATH):
     try:
-        answer = input("The Folder does not exist. Do you want to create it? [y] for Yes, or [n] for No ")
+        answer = input("The Back Up Folder does not exist. Do you want to create it? [y] for Yes, or [n] for No ")
+        # print(answer)
         if answer == "y":
             NAME_FILE = PATH_REPLICA.split("/")
             directory = NAME_FILE[-1]
@@ -60,7 +81,8 @@ def creationReplicaFolder(PATH_REPLICA):
             parent_dir = "/".join(PARENT_DIR)
             os.chdir(parent_dir)
             os.mkdir(directory)
-            print("The folder was created")
+            informationForLog(f"The folder {directory} was created", LOG_PATH)
+            print("The back up folder was created")
         else:
             print("Sorry we can not continue with the synchronization")
     except:
